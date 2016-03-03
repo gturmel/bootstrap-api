555982fb701b0f440734fa48c0bdfdfe5b3e0a08
Greg Turmel <gregturmel@gmail.com>
2016-03-02 21:42:48 -0500

    nothing works

---
+++ Assets/JS/script.js
@@ -0,0 +1,14 @@
+$(function(){
+   'use strict';
+
+var map;
+function initMap() {
+   console.log('works great');
+ map = new google.maps.Map($('#map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+ });//end of arguments for map
+}; // end of function initMap
+
+
+});//end of line
